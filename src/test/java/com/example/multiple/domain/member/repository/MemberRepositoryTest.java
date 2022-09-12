3aa14507d154c10e2653b5b27216aaa3968bf43c
99 <hahahoho5915@gmail.com>
2022-09-12 19:35:04 +0900

    modify @Transactional 트랜잭션 매니저 명시

--- src/test/java/com/example/multiple/domain/member/repository/MemberRepositoryTest.java
+++ src/test/java/com/example/multiple/domain/member/repository/MemberRepositoryTest.java
@@ -11,7 +11,7 @@ import java.util.Optional;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
-@Transactional
+@Transactional("memberTransactionManager")
 class MemberRepositoryTest {
 
 	@Autowired
