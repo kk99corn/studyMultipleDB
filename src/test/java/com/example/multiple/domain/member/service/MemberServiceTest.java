fbf5f7ac5f2ae2e783aa97b91e6f6a7096cc01bb
99 <hahahoho5915@gmail.com>
2022-09-16 18:57:55 +0900

    add Service 테스트 코드 추가

--- src/test/java/com/example/multiple/domain/member/service/MemberServiceTest.java
+++ src/test/java/com/example/multiple/domain/member/service/MemberServiceTest.java
@@ -1,7 +1,35 @@
 package com.example.multiple.domain.member.service;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.example.multiple.domain.member.dto.MemberDto;
+import com.example.multiple.domain.member.entity.Member;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+@SpringBootTest
+@Transactional("memberTransactionManager")
 class MemberServiceTest {
 
+	@Autowired
+	MemberService memberService;
+
+	@Test
+	void test() {
+		// Given
+		String id = "memberservicetest";
+		MemberDto memberDto = MemberDto.builder()
+				.id(id)
+				.build();
+
+		// When
+		memberDto = memberService.saveMember(memberDto);
+
+		// Then
+		assertThat(memberDto.getSeq()).isGreaterThan(0);
+		assertThat(memberDto.getId()).isEqualTo(id);
+	}
 }
